705b0b2ed1bdbca84a620256be6d89704fb8b686
nursah <nursahari@hotmail.com>
2021-12-25 22:42:09 +0300

    Initial Commit

---
+++ ThreadingProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThreadingProject
+//
+//  Created by Nurşah on 25.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
